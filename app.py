653eea15f8347c545ac8bef83f12a3da24833ceb
Rohan Prasad <thewarlock321@gmail.com>
2018-07-07 20:18:21 +0530

    Minor changes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, flash, redirect, request, session, abort
 import os
 import hashlib
 from pymongo import MongoClient
+from werkzeug.datastructures import ImmutableMultiDict
+
 app = Flask(__name__)
 
 
@@ -27,13 +29,14 @@ def signup():
 
 @app.route('/login', methods=['POST'])
 def login():
-    emp_id = request.json['employee_id']
-    print (emp_id)
-    password = request.json['password']
+
+    req = request.args.to_dict(flat=False)
+    emp_id = request.args['employee_id']
+    password = request.args['password']
     hash_password = hashlib.md5(password.encode()).hexdigest()
 
     user_cred = db['users'].find_one({"employee_id": str(emp_id)})
-    print (user_cred)
+
     #if the user exists in the database
     if user_cred != None:
 
@@ -55,6 +58,18 @@ def login():
         token = None
     return jsonify({'status': status, 'message': message, 'token': token})
 
+"""
+@app.route('/inputform', methods=['POST'])
+def inputform():
+
+    req = requests.args.to_dict(flat=False)
+    school_id = req["school_id"]
+    emp_id = req["employee_id"]
+
+    db['need_assessment'].insert_one("")
+    record = db['need_assessment'].find_one({"school_id": })
+
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
