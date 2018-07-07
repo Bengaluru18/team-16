8b558656b37512a203225276f70f0ccdce6d68bc
Rohan Prasad <thewarlock321@gmail.com>
2018-07-07 18:04:25 +0530

    Allow login.

--- app.py
+++ app.py
@@ -22,13 +22,39 @@ def signup():
     except:
         status = "500"
         message = "Unable to add the user."
-
     return jsonify({status: message})
 
 
-@app.route('/login', method=[POST])
+@app.route('/login', methods=['POST'])
 def login():
-    username =
+    emp_id = request.json['employee_id']
+    print (emp_id)
+    password = request.json['password']
+    hash_password = hashlib.md5(password.encode()).hexdigest()
+
+    user_cred = db['users'].find_one({"employee_id": str(emp_id)})
+    print (user_cred)
+    #if the user exists in the database
+    if user_cred != None:
+
+        # authenticate user
+        if hash_password == user_cred['password']:
+            # create a token of 16 bits to identify a session
+            token = os.urandom(16).hex()
+            db['logged_in_users'].insert_one({'employee_id': emp_id, 'token': token})
+            status = 200
+            message = "Successfully logged in."
+        else:
+            status = 401
+            message = "Invalid password"
+            token = None
+    else:
+
+        status = 404
+        message = "Employee id was not found."
+        token = None
+    return jsonify({'status': status, 'message': message, 'token': token})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
